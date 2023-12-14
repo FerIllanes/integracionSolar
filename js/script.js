ac926b4f193e67dcd79ec27e51dd86a0c9cf54a8
Fer Illanes <illanes.f.m@gmail.com>
2023-12-14 16:35:52 -0300

    cambios finales

--- js/script.js
+++ js/script.js
@@ -7,3 +7,23 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtiene todos los enlaces internos con fragmentos
+    const scrollLinks = document.querySelectorAll('a[href^="#"]');
+
+    // Recorre cada enlace y agrega un evento de clic
+    scrollLinks.forEach(function (link) {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            // Obtiene el destino del enlace (selecciona el elemento con el id correspondiente al href del enlace)
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            // Desplazamiento suave usando scrollIntoView
+            targetElement.scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+    });
+});
